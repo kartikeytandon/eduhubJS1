e9e3daefd61d674cde020592bfac1eeef5a49af1
Kartikey Tandon <tandonkartiey11@gmail.com>
2023-01-21 20:47:18 +0530

    Intro to JS

---
+++ index.js
@@ -0,0 +1,116 @@
+// // console.log("Hello world")
+
+// // Datatypes in JS
+// var a = 5
+
+// // let, const ES6
+
+// let b = 5
+
+// function to generate random value
+// let x = (Math.floor(Math.random() * 10)+1)
+// alert(x)
+
+// String, boolean, integer 
+// let z = 5
+// let m = "kartikey"
+
+// How to get current date and time in Javascript?
+
+// == / === / =
+
+// conditional operators in javascript?
+// if(x == 7) {
+//     alert("It is 7")
+// } else {
+//     alert("It is not 7")
+// }
+// let a = 5  copying the value in memory block 
+// a == 5  only comaring the data  
+// a === 5 comparing both data and datatype 
+
+// if (x == 1) {
+//     alert("Hello")
+// }
+// else if(x== 2) {
+//     alert("bye.")
+// }
+// else if(x == 3) {
+//     alert("ye.")
+// }
+// else if(x == 4) {
+//     alert("e.")
+// }
+// else {
+//     alert("It's not in the range of 1-4")
+// }
+
+// var x = 5
+// x == 2 ? console.log("It's 2") : console.log("It's not 2")
+
+// Sytax 
+// condition ? T : F
+
+// Looping in JS
+// for(initialization; condition; increment/decrement) {
+//     expression
+// }
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(1)
+// }
+
+// function in JS
+
+// function defination
+// function check() {
+//     var a = 5;
+//     var b = 7;
+//     console.log(a+b)
+// }
+// // function calling
+// check()
+
+// basic calculator
+
+// let a = prompt("Enter first value")
+// let b = prompt("Enter second value")
+
+// let choice = prompt("Enter your choice")
+
+// function add(a, b) {
+//     console.log(parseInt(a)+parseInt(b))
+// }
+// function subtract(a, b) {
+//     console.log(a-b)
+// }   
+// function multiply(a, b) {
+//     console.log(a*b)
+// }
+
+// if(choice == 1) {
+//     add(a, b);
+// }
+// else if(choice == 2) {
+//     subtract(a, b);
+// }
+// else if(choice == 2) {
+//     multiply(a, b);
+// }
+
+
+// let a = "edu"
+// let b = "hub"
+// console.log(a+b)
+
+// DOM 
+// document.getElementById("root").addEventListener()
+
+// let a = new Date()
+// alert(a)
+
+// Timer
+
+// function handleClick() {
+//     alert("I was clicked")
+// }
